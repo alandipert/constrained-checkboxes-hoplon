a5a73430abfcea160605f462c2a587968bdf1b0a
Alan Dipert <alan@dipert.org>
2017-05-01 14:22:22 -0700

    init

---
+++ src/pages/index.cljs
@@ -0,0 +1,31 @@
+(ns ^{:hoplon/page "index.html"} pages.index
+  (:require [clojure.pprint :as pp]
+            [hoplon.core    :as h :refer [div html head title body h2 fieldset label input for-tpl]]
+            [hoplon.jquery]
+            [javelin.core   :as j :refer [cell cell=]]
+            [linked.core    :as linked]))
+
+(defn choose
+  "Adds x to the linked set ls, ensuring that ls only retains the latest n
+  elements chosen so far."
+  [ls n x]
+  (if (> (count ls) (dec n))
+    (recur (disj ls (first ls)) n x)
+    (conj ls x)))
+
+(html
+  (head
+    (title "Constrained checkboxes in Hoplon"))
+  (body
+    (let [choices     ["Apple" "Apricot" "Banana" "Mango" "Orange" "Plum"]
+          max-choices 2
+          chosen      (cell (linked/set))]
+      (div
+        (h2 (pp/cl-format nil "Which fruits do you want? Pick ~R or fewer." max-choices))
+        (fieldset {:css {:border "none"}}
+          (for [choice choices]
+            (label :css {:display "block"}
+              (input {:type    "checkbox"
+                      :checked (cell= (contains? chosen choice))
+                      :change  #(swap! chosen choose max-choices choice)})
+              choice)))))))
